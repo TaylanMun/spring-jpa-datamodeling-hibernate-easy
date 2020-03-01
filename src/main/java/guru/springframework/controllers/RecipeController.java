39862d87d09722c104ac1d3b159bf6c963039fc1
Taylan Mun <tmun@maritimetrainer.com>
2020-03-01 21:01:39 +0300

    work notfound and badrequest page

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -54,10 +54,32 @@ public class RecipeController {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
-    public ModelAndView handleNotFound() {
-        log.error("Handling not found exception");
+    public ModelAndView handleNotFound(Exception exception) {
+
+        log.error("Handling Not Found Exception");
+        log.error(exception.getMessage());
+
         ModelAndView modelAndView = new ModelAndView();
+
         modelAndView.setViewName("404error");
+        modelAndView.addObject("exception", exception);
+
+        return modelAndView;
+    }
+
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public ModelAndView handleNumberFormat(Exception exception) {
+
+        log.error("Handling Number Format Exception");
+        log.error(exception.getMessage());
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("400error");
+        modelAndView.addObject("exception", exception);
+
         return modelAndView;
     }
 }
